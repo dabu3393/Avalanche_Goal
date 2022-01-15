5fcc1a40f27881b0ea8813b83ca1cb6d0dad339a
Davis Burrill <97137252+dabu3393@users.noreply.github.com>
2022-01-15 12:52:36 -0700

    Update avs_score_goal.py

--- avs_score_goal.py
+++ avs_score_goal.py
@@ -20,7 +20,7 @@ while True:
     avs_players_info = requests.get("https://statsapi.web.nhl.com/api/v1/teams/21?expand=team.roster")
     player_info = avs_players_info.json()
     game_id = score['dates'][0]['games'][0]['gamePk']
-    avs_stats = requests.get("https://statsapi.web.nhl.com/api/v1/game/" + str(game_id) + "/boxscore") # 2021020591 (<<<Blackhawks vs Avs Game ID, use to see stats from past game)
+    avs_stats = requests.get("https://statsapi.web.nhl.com/api/v1/game/" + str(game_id) + "/boxscore") # 2021020591 (<<<Blackhawks vs Avs Game ID, use to see stats from past game) 2021020606 (<<<Winnipeg Jets Game ID)
     stats = avs_stats.json()
 
     def home_away():
@@ -40,16 +40,14 @@ while True:
                         pass
                     else:
                         player_goal = stats['teams'][home_away()]['players'][id_string]['stats']['skaterStats']['goals']
-                        print(id_string)
-                        print(player_name + ": " + str(player_goal))
-
-
-    # Need to figure out how to return name of goal scorer
-
-
-
-
-    player_loop()
+                        if player_goal != 0 and player_goal != 1 and player_goal != 2:
+                            print("SCORING HIS " + str(player_goal) + "RD GOAL OF THE NIGHT, " + player_name.upper() + "!")
+                        elif player_goal != 0 and player_goal != 1:
+                            print("SCORING HIS " + str(player_goal) + "ND GOAL OF THE NIGHT, " + player_name.upper() + "!")
+                        elif player_goal != 0:
+                            print("SCORING HIS " + str(player_goal) + "ST GOAL OF THE NIGHT, " + player_name.upper() + "!")
+                        else:
+                            pass
 
 
 
@@ -61,18 +59,33 @@ while True:
 
 
     if home_team_name == "Colorado Avalanche" and home_team_score != starting_score:
-        print("AVS GOAL! Scored by ")
         os.system("afplay avs_goal_horn.wav&")
+        print("AVS GOAL!!!")
+        time.sleep(2)
+        player_loop() # Should print who scored, but will also print past scoring so need to fix that
+        time.sleep(2)
         print("New Score!!\nColorado Avalanche: " + str(home_team_score) + "\n" + away_team_name + ": " + str(away_team_score))
         starting_score = home_team_score
     elif away_team_name == "Colorado Avalanche" and away_team_score != starting_score:
-        print("AVS GOAL!")
         os.system("afplay avs_goal_horn.wav&")
+        print("AVS GOAL!!!")
+        time.sleep(2)
+        player_loop()  # Should print who scored, but will also print past scoring so need to fix that
+        time.sleep(2)
         print("New Score!!\nColorado Avalanche: " + str(away_team_score) + "\n" + home_team_name + ": " + str(home_team_score))
         starting_score = away_team_score
 
 
-    time.sleep(30)
+    time.sleep(3)
+
+
+# Issues that need fixed
+#     . Figure out how to print the current goal scorer and not all the goal scorers since it'll run through the whole loop picking up each player that has already scored
+#     . Since using two different API's to see who scored and what the score of the game is, make sure that the API's update their stats similtaneously so the player_loop() has data to present when scored
+#     .
+
+
+
 
 
 
